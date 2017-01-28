9f98722029c01a2aeaac3be1e2d5800de58f64fa
Phillip <pwg1994@gmail.com>
2017-01-27 20:09:30 -0500

    Completed.

--- 1degree.php
+++ 1degree.php
@@ -1,57 +1,54 @@
 <!DOCTYPE html>
 <html>
-	<head>
-        <title>My Movie Database (MyMDb)</title>
-		<meta charset="utf-8" />
+    <head>
+        <title>One Degree Of Seperation</title>
+        <meta charset="utf-8" />
 
-		<!-- Link to your CSS file that you should edit -->
-		<link href="bacon.css" type="text/css" rel="stylesheet" />
-	</head>
+        <!-- Link to your CSS file that you should edit -->
+        <link href="bacon.css" type="text/css" rel="stylesheet" />
+    </head>
 
-	<body>
-
-<?php
-
-$firstname =  $_GET['firstname'];
-$lastname =  $_GET['lastname'];
-
-$servername = "127.0.0.1";
-$username = "root";
-$password = "password";
-
-// Create connection
-$conn = mysql_connect($servername, $username, $password);
-if(! $conn){
-    die("Database connection failed: ".mysql_error());
-}
-
-$db_select = mysql_select_db("myDB", $conn);
-if(! $db_select){
-    die("Database conn failed: ".mysql_error());
-}
-else{
-    $query = "SELECT * FROM myDB.actors WHERE first_name = '" . $firstname . "' AND last_name = '" . $lastname . "'";
-    $result = mysql_query($query);
-    if($result){
-    $row = mysql_fetch_assoc($result);
-    
-    $query = "SELECT * FROM myDB.roles, myDB.movies WHERE myDB.roles.movie_id = myDB.movies.id AND actor_id = " . $row['id'];
-    $result = mysql_query($query);
-    if($result){
-    	echo '<table>';
-    	while($row = mysql_fetch_assoc($result)){
-    		echo '<tr><td>' . $row['name'] . '</td><td>'. $row['year'] . '</td><td>' . $row['rank'] . '</td></tr>';
-    	}
-    	echo '</table>';
-    } else {
-    	echo "err";
-    }
-} else {
-	echo "err";
-}
-}
-
-?>
-
-	</body>
+    <body>
+        <div id="banner">
+            <a href="index.php"></a>
+        </div>
+        <div id="content">
+            <h1>Results for <?php echo $_GET['firstname'] . ' ' . $_GET['lastname']; ?> (one degree of Kevin Bacon) </h1>
+            <?php
+//            Line below connects to database, done in 1degree.php
+            include '1degree.php';
+//            Lines below set user input to variables $firstname and $lastname
+            $firstname = $_GET['firstname'];
+            $lastname = $_GET['lastname'];
+            // Create connection
+            try {
+                $conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                // function below returns actor_id, from 1degree.php
+                $actorid = get_actor_id($firstname, $lastname);
+                if ($actorid === -1) {
+                    echo "Actor " . $firstname . " " . $lastname . " not found";
+                    exit;
+                }
+                // query below returns list of movies in which Kevin Bacon and other actor has starred in
+                $query = "SELECT * FROM movies m JOIN roles r ON r.movie_id = m.id JOIN actors a ON r.actor_id = a.id JOIN roles rr ON rr.movie_id = m.id JOIN actors aa ON rr.actor_id = aa.id WHERE r.movie_id = rr.movie_id AND r.actor_id = '" . $actorid . "' AND rr.actor_id = " . $kevinsid . " ORDER BY m.year DESC, m.name ASC";
+                $stmt = $conn->prepare($query);
+                $stmt->execute();
+                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                echo '<table><tr><th>#</th><th>Title</th><th>Year</th></tr>';
+                $index = 1;
+                // loop below prints out results in html
+                while ($row = $stmt->fetch()) {
+                    echo '<tr><td>' . $index . '</td><td>' . $row["name"] . '</td><td>' . $row['year'] . '</td></tr>';
+                    $index++;
+                }
+                echo '</table>';
+            } catch (PDOException $e) {
+                die('Database connection failed: ' . $e->getMessage());
+            }
+            $conn = null;
+            ?>
+            <p>A table showing movies that this actor has starred in that also starred Kevin Bacon.</p>
+        </div>
+    </body>
 </html>
